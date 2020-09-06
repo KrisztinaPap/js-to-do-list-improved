3f3c413c7df94279198c8f988eccdf7412afdaa7
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-06 11:53:52 -0600

    Adds delete button functionality;

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 // Declare variables
 const addTaskForm = document.getElementById( "add-task-form" );
 const addTaskButton = document.getElementById( "add-task-button" );
@@ -5,6 +7,7 @@ const pendingTaskArea = document.getElementById( "pending-task-list-area" );
 const activeTaskArea = document.getElementById( "active-task-list-area" );
 const completedTaskArea = document.getElementById( "completed-task-list-area" );
 const interestingFactsArea = document.getElementById( "interesting-facts-area" );
+const deleteButtons = document.querySelectorAll( ".deleteButton" );
 
 // Event listener on add button
 addTaskButton.addEventListener( "click", addTask );
@@ -19,20 +22,27 @@ function addTask( event ) {
     // Add "start" button
     const newStartButton = document.createElement( "BUTTON" );
     newStartButton.textContent = "Start";
+    newStartButton.classList.add( "startButton" );
     newLI.append( newStartButton );
 
     // Add "delete" button
     const newDeleteButton = document.createElement( "BUTTON" );
     newDeleteButton.textContent = "Delete";
+    newDeleteButton.classList.add( "deleteButton" );
+    newDeleteButton.addEventListener( "click", deleteTask );
     newLI.append( newDeleteButton );
 
     pendingTaskArea.appendChild( newLI );
-};
+}; 
 
 //      startTask()
 
 //      completeTask()
 
 //      deleteTask()
+function deleteTask( event ) {
+  this.parentElement.remove();
+}
+
 
 //      clearList()
