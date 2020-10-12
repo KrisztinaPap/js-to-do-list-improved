4278e775a4d5e6f9c1ea7a0e2025c38bcacb019d
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-12 13:29:08 -0600

    Adds trim to userinput; Only adds new task if userinput is not empty;

--- js/scripts.js
+++ js/scripts.js
@@ -21,68 +21,72 @@ clearListButton.addEventListener( "click", clearList );
 //      addTask()
 function addTask( event ) {
     event.preventDefault();
-    const userInput = document.getElementById( "taskName" ).value;
+    const userInput = document.getElementById( "taskName" ).value.trim();
     const newLI = document.createElement( "LI" );
   
-    // Add text to li
-    const newTextContent = document.createElement( "SPAN" );
-    newTextContent.textContent = userInput;
-    newTextContent.classList.add( "LIText" );
-    newLI.appendChild( newTextContent );
+    if ( userInput !== "" )
+    {
+      // Add text to li
+      const newTextContent = document.createElement( "SPAN" );
+      newTextContent.textContent = userInput;
+      newTextContent.classList.add( "LIText" );
+      newLI.appendChild( newTextContent );
 
-    // Add "start" button
-    const newStartButton = document.createElement( "BUTTON" );
-    newStartButton.textContent = "Start";
-    newStartButton.classList.add( "startButton", "yellow", "button" );
-    newStartButton.addEventListener( "click", startTask );
-    newLI.append( newStartButton );
+      // Add "start" button
+      const newStartButton = document.createElement( "BUTTON" );
+      newStartButton.textContent = "Start";
+      newStartButton.classList.add( "startButton", "yellow", "button" );
+      newStartButton.addEventListener( "click", startTask );
+      newLI.append( newStartButton );
 
-    // Add "delete" button
-    const newDeleteButton = document.createElement( "BUTTON" );
-    newDeleteButton.textContent = "Delete";
-    newDeleteButton.classList.add( "deleteButton", "red", "button" );
-    newDeleteButton.addEventListener( "click", deleteTask );
-    newLI.append( newDeleteButton );
+      // Add "delete" button
+      const newDeleteButton = document.createElement( "BUTTON" );
+      newDeleteButton.textContent = "Delete";
+      newDeleteButton.classList.add( "deleteButton", "red", "button" );
+      newDeleteButton.addEventListener( "click", deleteTask );
+      newLI.append( newDeleteButton );
 
-    pendingTaskArea.appendChild( newLI );
+      pendingTaskArea.appendChild( newLI );
+  }
+
 }; 
 
 //      startTask()
 function startTask( event ) {
-  activeTaskArea.appendChild( this.parentElement );
-  //const thisStartButton = document.querySelector("");
-  
-  // Add IsCompleted checkbox
-  const newCheckbox = document.createElement( "INPUT");
-  newCheckbox.type = "checkbox";
-  
-  newCheckbox.addEventListener( "change", completeTask );
-  this.parentElement.prepend( newCheckbox );
-  this.remove();
+    activeTaskArea.appendChild( this.parentElement );
+    //const thisStartButton = document.querySelector("");
+    
+    // Add IsCompleted checkbox
+    const newCheckbox = document.createElement( "INPUT");
+    newCheckbox.type = "checkbox";
+    
+    newCheckbox.addEventListener( "change", completeTask );
+    this.parentElement.prepend( newCheckbox );
+    this.remove();
 }
 
 //      completeTask()
 function completeTask( event ) {
-  if ( this.checked )
-    {
-      completedTaskArea.appendChild( this.parentElement ); 
-    } 
-  else {
-      activeTaskArea.appendChild( this.parentElement );
-    }
+    if ( this.checked )
+        {
+          completedTaskArea.appendChild( this.parentElement ); 
+        } 
+    else {
+          activeTaskArea.appendChild( this.parentElement );
+        }
 }
 
 //      deleteTask()
 function deleteTask( event ) {
-  this.parentElement.remove();
+    this.parentElement.remove();
 }
 
 //      clearList()
 function clearList() {
-  let allChecked = document.querySelectorAll("ul li");
-  for ( let i = 0; i < allChecked.length; i++ ) {
-    if ( allChecked[i].firstChild.checked ) {
-      allChecked[i].remove();
+    let allChecked = document.querySelectorAll("ul li");
+    for ( let i = 0; i < allChecked.length; i++ ) {
+        if ( allChecked[i].firstChild.checked ) {
+            allChecked[i].remove();
+        }
     }
-  }
 }
